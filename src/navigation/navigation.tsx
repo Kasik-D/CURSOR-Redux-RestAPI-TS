5a2245431053718b55f8e0076103caf8359dab93
Danil Kasianenko <danildanilkas1@gmail.com>
2023-08-28 15:19:36 +0300

    delete(navigation.tsx): delete unused code

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,17 +1,11 @@
 import { AnimeDetails, Home, NotFoundPage, Root } from 'pages/index';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-// import { fetchGenres } from 'store/slices/home.slicer';
-
-// import { useAppDispatch } from 'store/store';
 
 export const RoutersProvider = () => {
-  // const dispatch = useAppDispatch();
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<Home />} />
-        {/* <Route index element={<Home />} loader={() => dispatch(fetchGenres())} />  */}
         <Route path="anime" element={<AnimeDetails />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>,
